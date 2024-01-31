fe3a080e9768ad038722965951478fcf6138b318
butch0012 <elnathanmburu@gmail.com>
2024-01-31 13:04:48 +0300

    done

--- server/app.py
+++ server/app.py
@@ -14,13 +14,38 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    messages_list = [{"id": message.id, "body": message.body, "username": message.username, "created_at": str(message.created_at), "updated_at": str(message.updated_at)} for message in messages]
+    return jsonify(messages_list)
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.json
+    new_message = Message(body=data['body'], username=data['username'])
+    db.session.add(new_message)
+    db.session.commit()
+    return jsonify({"id": new_message.id, "body": new_message.body, "username": new_message.username, "created_at": str(new_message.created_at), "updated_at": str(new_message.updated_at)})
 
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = Message.query.get(id)
+    if message:
+        data = request.json
+        message.body = data['body']
+        db.session.commit()
+        return jsonify({"id": message.id, "body": message.body, "username": message.username, "created_at": str(message.created_at), "updated_at": str(message.updated_at)})
+    return jsonify({"error": "Message not found"}), 404
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.get(id)
+    if message:
+        db.session.delete(message)
+        db.session.commit()
+        return jsonify({"message": "Message deleted successfully"})
+    return jsonify({"error": "Message not found"}), 404
 if __name__ == '__main__':
     app.run(port=5555)
